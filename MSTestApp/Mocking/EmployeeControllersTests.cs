bcaf875f276aaaa76746128cc6ad8d1550bb44c3
Chris Shaw <chris1108@hotmail.co.uk>
2018-04-11 14:27:18 +0100

    More tests using common methods

---
+++ MSTestApp/Mocking/EmployeeControllersTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace MSTestApp.Mocking
+{
+    [TestFixture]
+    class EmployeeControllersTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            storage.Verify(s => s.DeleteEmployee(1));
+
+        }
+    }
+}
